68bfdfce3410942080a3018bfd9ece5047be59b3
Staffan Olsson <staffan@repos.se>
2017-11-24 13:09:46 +0100

    Shows a decoded jwt token, if env JWT_HEADER is set

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require('express')
 const morgan = require('morgan');
 var app = express()
-const os = require('os')
+const os = require('os');
+const jwt = require('jsonwebtoken');
 
 app.set('json spaces', 2);
 
 app.use(morgan('combined'));
 
 app.all('*', (req, res) => {
-  res.json({
+  const echo = {
     path: req.path,
     headers: req.headers,
     method: req.method,
@@ -25,7 +26,17 @@ app.all('*', (req, res) => {
     os: {
       hostname: os.hostname()
     }
-  })
+  };
+  if (process.env.JWT_HEADER) {
+    const token = req.headers[process.env.JWT_HEADER.toLowerCase()];
+    if (!token) {
+      echo.jwt = token;
+    } else {
+      const decoded = jwt.decode(token, {complete: true});
+      echo.jwt = decoded;
+    }
+  }
+  res.json(echo);
 })
 
 app.listen(process.env.PORT || 80)
